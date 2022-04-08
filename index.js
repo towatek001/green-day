c92d6b8527c054175ae6fc69dae65ea7212b0a67
Tony Wang <tony@Tonys-MBP.cgocable.net>
2022-04-07 22:05:02 -0400

    add static file hosting

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
-import Fastify from 'fastify'
+import Fastify from 'fastify';
+import fastifyStatic from 'fastify-static';
+import path from 'path';
+import {fileURLToPath} from 'url';
 
 const PORT = process.env.PORT || 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const fastify = Fastify({
   logger: true
 });
 
+fastify.register(fastifyStatic, {
+  root: path.join(__dirname, 'public'),
+  // prefix: '/public/', // optional: default '/'
+})
+
 fastify.get('/', async (request, reply) => {
-  return { hello: 'world' };
+  return reply.sendFile('index.html');
+  // return { hello: 'world' };
 })
 
 const start = async () => {
